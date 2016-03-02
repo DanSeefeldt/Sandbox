e502156c0863c674406fc316dc017840b32fe23a
Dan Seefeldt <DanSeefeldt@outlook.com>
2016-03-02 16:10:39 -0600

    Made some changes to the text file and the app.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Hello Docker!");
+            }
         }
     }
 }
