46ff3a1bda9fb67b8bca5ca502ab0ada520672f6
Dan Seefeldt <DanSeefeldt@outlook.com>
2016-03-24 19:58:55 +0000

    Adding copy files script

---
+++ xfer/copyfiles.sh
@@ -0,0 +1,7 @@
+cp /opt/Sandbox/xfer/HelpCommand.cs src/dotnet/commands/dotnet-help/
+cp /opt/Sandbox/xfer/Program.cs src/dotnet/
+cp /opt/Sandbox/xfer/CommandContext.cs src/Microsoft.DotNet.Cli.Utils/
+cp /opt/Sandbox/xfer/Env.cs src/Microsoft.DotNet.Cli.Utils/
+cp /opt/Sandbox/xfer/Product.cs src/Microsoft.DotNet.Cli.Utils/
+cp /opt/Sandbox/xfer/project.json src/Microsoft.DotNet.Cli.Utils/
+cp /opt/Sandbox/xfer/Telemetry.cs src/Microsoft.DotNet.Cli.Utils/
